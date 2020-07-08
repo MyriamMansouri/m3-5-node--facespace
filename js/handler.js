dae8816fe4d96b9bb18d542b0476ab1574b13736
myriam mansouri <myriam.mansouri@gmail.com>
2020-07-08 17:39:08 -0400

    Exercise 1 done

--- js/handler.js
+++ js/handler.js
@@ -1,9 +1,11 @@
+const { users } = require("../data/users");
+
 const handleHomepage = (req, res) => {
-  res.status(200).render("pages/homepage");
+  res.status(200).render("pages/homepage", { users });
 };
 
 const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
 };
 
-module.exports = { handleHomepage, handleFourOhFour }
+module.exports = { handleHomepage, handleFourOhFour };
